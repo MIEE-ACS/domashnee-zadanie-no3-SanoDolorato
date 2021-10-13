f039c4dc870b03f7dfa1699888126fce83062fbc
SanoDolorato <90412284+SanoDolorato@users.noreply.github.com>
2021-10-14 00:59:52 +0300

    создал

---
+++ domashnee-zadanie-no3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace domashnee_zadanie_no3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            List<char> alphavit = new List<char> { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я' };
+            
+            string a = tbInput.Text;
+            string result = a.ToString();
+            string newresult = "";
+            for(int i=0;i<result.Length;i++)
+            {
+                int k = 0;
+                while(result[i]!=alphavit[k])
+                {
+                    k++;
+                }
+                newresult += alphavit[alphavit.Count-k-1];
+            }
+            tbOutput.Text = newresult;
+
+        }
+    }
+}
