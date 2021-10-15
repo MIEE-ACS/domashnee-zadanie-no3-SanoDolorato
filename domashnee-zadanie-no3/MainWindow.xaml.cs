3e7dedb96df4787f75da16c19f7483a3313e6b7b
SanoDolorato <90412284+SanoDolorato@users.noreply.github.com>
2021-10-15 23:40:08 +0300

    Дополнительно
    Добавил английский язык

--- domashnee-zadanie-no3/MainWindow.xaml.cs
+++ domashnee-zadanie-no3/MainWindow.xaml.cs
@@ -28,11 +28,15 @@ namespace domashnee_zadanie_no3
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
             List<char> alphavit = new List<char> { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я' };
-
+            List<char> alphavit2 = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
             string a = tbInput.Text;
             string result = a.ToString();
             string newresult = "";
-            newresult = Atbash(alphavit, result, newresult);
+            //надо понять, слово на русском или на английском
+            if (alphavit2.Contains(result[0])) 
+                newresult = Atbash(alphavit2, result, newresult);
+            else
+                newresult = Atbash(alphavit, result, newresult);
             tbOutput.Text = newresult;
 
         }
